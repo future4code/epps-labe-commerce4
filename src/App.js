0ac640ef01d7e8a58a6419612bbed24fa9228050
Carolaine Viana <carolaine.viana@MacBook-Pro-de-Carolaine.local>
2021-01-07 13:35:04 -0300

    stylizacao do comprar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import './App.css';
 import styled from 'styled-components'
-import Carrinho from './components/Carrinho'
 
-const DivMain = styled.div`
-    width: 300px;
-    height: 300px;
-    border: 1px solid black;
-  `
 const DivFiltro = styled.div`
   width: 300px;
   height: 500px;
@@ -33,7 +27,6 @@ const CaixaImagem = styled.div`
   width: 200px;
   height: 350px;
   border: 1px solid black;
-
 `
 
 const BotaoCompra = styled.button`
@@ -48,112 +41,167 @@ const DivCarrinho = styled.div`
     flex-direction: column;
     margin-top: 10px;
     width: 800px;
-    height: 500px;
-    border: 1px solid black;
+    height: 600px;
+    border: 1px dotted black;
 `
-const soma = 0
-let total = 0
+const DeletarItem =  styled.button`
+  width: 50px;
+  height: 20px;
+`
+
+const CaixaProduto = styled.div`
+  text-align: center;
+  width: 200px;
+  height: 200px;
+  border-style: dotted;
+  border-width: 0px 0px 2px 0px; /* 25px top, 10px right, 4px bottom and 35px left */
+`
+
+const BotaoComprar = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+  background-color: red;
+`
 
 export default class App extends React.Component {
 
   state = {
-    carrinho: [],
     produtos: [{
       id: 1,
       nome: "teste1",
       value: 85,
+      quantidade: 0,
+      subTotal:0,
       imageUrl: "https://picsum.photos/id/3/200/200"
     },
-  {
-    id: 2,
+    {
+      id: 2,
       nome: "teste2",
       value: 85,
+      quantidade: 0,
+      subTotal:0,
       imageUrl: "https://picsum.photos/id/2/200/200"
-  },
-  {
-    id: 3,
-    nome: "teste3",
-    value: 150,
-    imageUrl: "https://picsum.photos/id/5/200/200"
-  }
-]
+    },
+    {
+      id: 3,
+      nome: "teste3",
+      value: 150,
+      quantidade: 0,
+      subTotal:0,
+      imageUrl: "https://picsum.photos/id/5/200/200"
+    },
+    ], 
+    carrinho: [],
+    carrinhoMostrar: true
   }
 
-selecionarProduto = (id) =>{
-  const item = this.state.produtos.map((p)=>{
-    if(id===p.id){
-       this.setState({
-         carrinho: [...this.state.carrinho, p]
-       })
+  excluirItem = (item) => {
+    const listaProdutos = this.state.carrinho.filter((p) => {
+      return p.id !== item.id;
+    });
+
+    this.setState({ carrinho: listaProdutos });
+  };
+
+
+  aoClicarNoCarrinho = () => {
+    this.setState({ carrinhoMostrar: !this.state.carrinhoMostrar });
+  };
+
+  selecionarProduto = (id) => {
+
+    let novoCarrinho = [...this.state.carrinho]
+
+    const existeProduto = this.state.produtos.find((p) => {
+      if (id === p.id) {
+        return true
+      }
+      return false
+    })
+
+    const existeProdutoNoCarrinho = novoCarrinho.find((item) => {
+      if (id === item.id) {
+        return true
+      }
+      return false
+    })
+
+    if (existeProdutoNoCarrinho === undefined) {
+      const novoProduto = {
+        ...existeProduto,
+        quantidade: 1,
+        subTotal: existeProduto.value,
+      }
+      novoCarrinho = [novoProduto, ...novoCarrinho]; 
+    } else {
+      novoCarrinho = novoCarrinho.map((item) => {
+        if (id === item.id) {
+          return {
+            ...item,
+            quantidade: item.quantidade + 1, 
+            subTotal: item.value + item.subTotal,
+          }
+        } else {
+          return item
+        }
+      });
     }
-    console.log('aqui pai', this.state.carrinho.length)
-    soma = this.state.carrinho.length
-    console.log(soma)
-  })
- console.log(this.state.carrinho)
-}
+    this.setState({ carrinho: novoCarrinho })
+  }
 
-
-// addProduto = () =>{
-//     const novoarray = [...this.state.carrinho]
-//     console.log(novoarray)
-//     const index = this.state.carrinho.findIndex((item) => item.carrinho.id === carrinho.id)
-//     console.log(index)
-// }
-
-totalCarrinho=()=>{
-  let total=0
-  this.state.carrinho.map((item) =>{
-    total+=Number(item.value)
-    console.log(item.value)
-  })
-  return total
-  
-  
-}
+  totalCarrinho = () => {
+    let total = 0
+    this.state.carrinho.map((item) => {
+      total += item.subTotal
+    })
+    return total
+  }
   render() {
     const tamanhoProdutos = this.state.produtos.length
 
+    const exibirCarrinho = () => {
+      const itensDoCarrinho = this.state.carrinho.map((p) => {
+        return (
+          <CaixaProduto>
+            <p> <strong>Quantidade:</strong>{p.quantidade}x</p>
+            <p><strong>Produto:</strong>{p.nome}</p>
+            <p><strong>Total:</strong>R$ {p.subTotal} </p>
+            <p onClick={()=>{this.excluirItem(p)}}> <DeletarItem>Deletar</DeletarItem> </p>
+          </CaixaProduto>
+        )
+      })
+      return itensDoCarrinho;
+    }
     return (
       <main>
+        <BotaoComprar onClick={this.aoClicarNoCarrinho}/>
         <DivFiltro>
           <h3>Filtros:</h3>
           <label>Valor minimo</label>
-          <InputMinimo type="number" value="teste"/>
+          <InputMinimo type="number" value="teste" />
           <label>Buscar Produto</label>
-          <InputProduto type="text"/>
+          <InputProduto type="text" />
         </DivFiltro>
 
         <h3>Quantidade {tamanhoProdutos}</h3>
-        {this.state.produtos.map(p=>{
-                    return (
-                      
-                        <DivProdutos>
-                          <CaixaImagem>
-                          <img src={p.imageUrl}/>
-                          <p>{p.nome}</p>
-                          <p>R${p.value}</p>
-                          <BotaoCompra onClick={()=>this.selecionarProduto(p.id)}>Adicionar carinho</BotaoCompra>
-                          </CaixaImagem>
-                        </DivProdutos>
-                    )
-                })}
-                {/* <Carrinho/> */}
-                <DivCarrinho>
-                  
-                {this.state.carrinho.map(x=>{
-                    return (
-                      
-                      <div>
-                        
-                        <p>{x.id}</p>
-                        <p>Produto: {x.nome}</p>
-                        <p>Valor: {x.value}</p>
-                      </div>
-                    )
-                })}
-                <p>Total: {this.totalCarrinho()}</p>
-                </DivCarrinho>
+        {this.state.produtos.map(p => {
+          return (
+
+            <DivProdutos>
+              <CaixaImagem>
+                <img src={p.imageUrl} />
+                <p>{p.nome}</p>
+                <p>R${p.value}</p>
+                <BotaoCompra onClick={() => this.selecionarProduto(p.id)}>Adicionar ao carinho</BotaoCompra>
+              </CaixaImagem>
+            </DivProdutos>
+          )
+        })}
+        <DivCarrinho>
+          {exibirCarrinho()}
+          <p>Total: {this.totalCarrinho()}</p>
+        </DivCarrinho>
       </main>
     )
   }
